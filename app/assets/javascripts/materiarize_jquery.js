2fa4b9e9a9136df587331f7d649a811028dc4195
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-10-26 14:24:45 +0900

    generated top page

---
+++ app/assets/javascripts/materiarize_jquery.js
@@ -0,0 +1,6 @@
+$(document).on('turbolinks:load', function () {
+  $('.sidenav').sidenav({
+    edge: "right"
+  });
+  $('.parallax').parallax();
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_135551) do
+ActiveRecord::Schema.define(version: 2019_10_22_145605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sheets", force: :cascade do |t|
+    t.string "title"
+    t.boolean "is_public"
+    t.boolean "is_cooperate"
+    t.boolean "is_secret"
+    t.boolean "is_pulled"
+    t.string "pulling_number"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sheets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_10_22_135551) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sheets", "users"
 end
