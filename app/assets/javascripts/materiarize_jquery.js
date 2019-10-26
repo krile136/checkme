17b1bca766a715056d649c5418845fd67c184600
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-10-26 19:50:53 +0900

    marked up sign in page

--- app/assets/javascripts/materiarize_jquery.js
+++ app/assets/javascripts/materiarize_jquery.js
@@ -2,7 +2,11 @@ $(document).on('turbolinks:load', function () {
   $('.sidenav').sidenav({
     edge: "right"
   });
-
   $('.parallax').parallax();
+});
 
+// ページが遷移する際に、sidenavをリセットする
+// リセットしないとsidenavが動かない
+$(document).on("turbolinks:before-cache", function () {
+  $('.sidenav').sidenav('destroy');
 });
