5c75c4186d320e788f4ee6c33131d4883eaff9ec
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-11-07 12:37:33 +0900

    small fix

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,4 +1,15 @@
 class SheetsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
+
+  end
+
+  def new
+
+  end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
   end
 end
