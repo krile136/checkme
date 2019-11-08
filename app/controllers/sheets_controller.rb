f1b7d22ec5f2d3447bda3b76fac7962423108bd2
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-11-08 16:59:29 +0900

    Enabled to get sheets

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -12,7 +12,6 @@ class SheetsController < ApplicationController
 
   def create
     @sheet = Sheet.new(sheet_params)
-    binding.pry
     if @sheet.save
       # redirect_to root_path, notice: '出品が完了しました'
       redirect_to root_path
@@ -30,6 +29,6 @@ class SheetsController < ApplicationController
   def sheet_params
     params.require(:sheet).permit(:title,
                   items_attributes:[:name, :is_head, :top])
-                  .merge(user_id: current_user.id).merge(pulling_number: 0).merge(last_view: Time.now + 60*60*9)
+                  .merge(user_id: current_user.id).merge(pulling_number: 0).merge(last_view: Time.now - (60 * 60 * 24 * 5))
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,25 @@ class UsersController < ApplicationController
     
   end
 
+  def show
+
+    # 今の時間を取得（データベース用に修正）
+    today = Time.now
+
+    # 昨日までの時間を取得（データベース用に修正）
+    one_day = 60 * 60 * 24
+    yesterday = today - one_day
+
+    # １週間の期間を取得（データベース用に修正）
+    one_week = today - (one_day * 7)
+    
+    @today_sheets = Sheet.where(user_id: current_user.id).where(last_view: yesterday..today)
+    @week_sheets = Sheet.where(last_view: one_week..yesterday)
+
+    binding.pry
+
+  end
+
   private
 
   def move_to_index
