047a11dbef9a63944b35b98c920b85d5b02cfcd6
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-11-19 22:59:47 +0900

    added show-howto column to user table

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'sheets#index'
 
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    collection do
+      get :how_to_use
+    end
+  end
+
   resources :sheets, only: [:index, :new, :create, :edit, :update, :destroy, :show] do
     member do
       patch :update_date
@@ -14,13 +19,16 @@ Rails.application.routes.draw do
       get :get_check
     end
   end
+
   resources :items, only: [:update, :show]
+
   resources :cooperate_requests, only: [:create, :destroy] do
     member do
       delete :reject
       patch :accept
     end
   end
+
   namespace :api do
     resources :sheets, only: :index, defaults: { format: 'json' } do
       collection do
@@ -30,4 +38,5 @@ Rails.application.routes.draw do
     end
     resources :users, only: [:index, :update], defaults: { format: 'json' }
   end
+  
 end
