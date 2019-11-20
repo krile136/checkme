57b55a02d8754ff97cb509f258cbc9b29e336db8
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-11-20 21:42:44 +0900

    added back button

--- app/assets/javascripts/action_help.js
+++ app/assets/javascripts/action_help.js
@@ -2,10 +2,17 @@ $(document).on('turbolinks:load', function () {
   $('.help-make').on('click', function (e) {
     e.preventDefault();
 
+    $('.modal-back').css('display', 'inline-block');
+
     $('#help_list').css('display', 'none');
     $('#help_make_content').css('display', 'block');
 
 
   })
-
+  $('.modal-back').on('click', function (e) {
+    e.preventDefault();
+    $('.modal-back').css('display', 'none');
+    $('.help_back_content').css('display', 'none');
+    $('#help_list').css('display', 'block');
+  })
 })
