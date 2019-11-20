7fdb9ed01e0aa86a8047b7b1f28f3338af7dd510
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-11-20 21:22:18 +0900

    added help make

--- app/assets/javascripts/action_help.js
+++ app/assets/javascripts/action_help.js
@@ -1,4 +1,11 @@
 $(document).on('turbolinks:load', function () {
+  $('.help-make').on('click', function (e) {
+    e.preventDefault();
 
+    $('#help_list').css('display', 'none');
+    $('#help_make_content').css('display', 'block');
+
+
+  })
 
 })
