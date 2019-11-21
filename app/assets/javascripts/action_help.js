a7f1e93063a74b8d36efbd9ce76b01b5ddfe21cc
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-11-22 00:41:14 +0900

    added how to make sheet public

--- app/assets/javascripts/action_help.js
+++ app/assets/javascripts/action_help.js
@@ -35,6 +35,33 @@ $(document).on('turbolinks:load', function () {
     $('#help_public_content').css('display', 'block');
   })
 
+  $('.help-search-mypage').on('click', function (e) {
+    e.preventDefault();
+
+    $('.modal-back').css('display', 'inline-block');
+
+    $('#help_list').css('display', 'none');
+    $('#help_search_mypage_content').css('display', 'block');
+  })
+
+  $('.help-search-public').on('click', function (e) {
+    e.preventDefault();
+
+    $('.modal-back').css('display', 'inline-block');
+
+    $('#help_list').css('display', 'none');
+    $('#help_search_public_content').css('display', 'block');
+  })
+
+  $('.help-pull').on('click', function (e) {
+    e.preventDefault();
+
+    $('.modal-back').css('display', 'inline-block');
+
+    $('#help_list').css('display', 'none');
+    $('#help_pull_content').css('display', 'block');
+  })
+
   $('.modal-back').on('click', function (e) {
     e.preventDefault();
     $('.modal-back').css('display', 'none');
