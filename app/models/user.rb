cfc880175de8d71ffac8452851f700c98ee842dd
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-11-26 23:12:17 +0900

    generated guest login routing and controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
   has_many :sheets, through: :user_sheets, dependent: :destroy
 
   has_many :cooperate_requests, dependent: :destroy
+
+  def self.guest
+    find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'sheets#index'
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
   resources :users, only: [:index, :show, :edit, :update]
 
   resources :sheets, only: [:index, :new, :create, :edit, :update, :destroy, :show] do
